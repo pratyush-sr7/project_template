1b625783609129e19464eebf9f5f7d5eec1b340c
Pratyush Srivastava <154667812+pratyush-sr7@users.noreply.github.com>
2023-12-28 23:13:42 +0530

    Update snowflake_connector.py
    Snowflake Connector

--- snowflake_connector.py
+++ snowflake_connector.py
@@ -4,6 +4,13 @@ import snowflake.connector
 
 load_dotenv()
 
+SNOWFLAKE_HOST = os.getenv("SNOWFLAKE_HOST")
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_ROLE = os.getenv("SNOWFLAKE_ROLE")
+SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
+
 class Snowflake:
   con = snowflake.connector.connect(host=SNOWFLAKE_HOST,account=SNOWFLAKE_ACCOUNT,user=SNOWFLAKE_USER,password=SNOWFLAKE_PASSWORD,role=SNOWFLAKE_ROLE,warehouse=SNOWFLAKE_WAREHOUSE)
   cur = con.cursor()
